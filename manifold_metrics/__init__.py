875bbdbadcb2f73d90f526f897c4b6cf16a1a833
antongomez <anton.gomez.lopez@rai.usc.es>
2025-12-04 14:14:24 +0100

    Load judge model weights with load state

--- manifold_metrics/__init__.py
+++ manifold_metrics/__init__.py
@@ -2,6 +2,7 @@
 Utilities to compute manifold metrics for multispectral images.
 """
 
+from .cnn2d_residual import HYPERPARAMS, CNN2D_Residual, DatasetMock
 from .manifold_metrics import calculate_fid, compute_precision_recall
 
 __version__ = "1.0.0"
@@ -10,4 +11,7 @@ __author__ = "Antón Gómez López"
 __all__ = [
     "calculate_fid",
     "compute_precision_recall",
+    "CNN2D_Residual",
+    "DatasetMock",
+    "HYPERPARAMS",
 ]
